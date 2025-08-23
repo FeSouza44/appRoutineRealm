a10d8d0be305d4d6d704ae40faa63fdb97467234
Felipe <feamaral.comercial@gmail.com>
2025-08-23 17:12:26 -0300

    adiconando arquivos model e controller

---
+++ src/model/Tarefa.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Tarefa {
+    private int id;
+    private String nome;
+    private String descricao;
+    private Timestamp Inicio;
+    private Timestamp Fim; 
+    
+}
---
+++ src/model/Usuario.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Usuario {
+    private int id;
+    private String email, senha;
+    public  String nome;
+
+    public Usuario(String senha, String email, String nome) {
+        this.senha = senha;
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
